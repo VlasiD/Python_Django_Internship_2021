a82601ed3e3968e064e99829c6bf65b03ae62a8d
Vladimir <vladimirsidenko11@gmail.com>
2021-12-12 18:16:16 +0200

    Fixed typos.

--- lesson2.py
+++ lesson2.py
@@ -11,7 +11,7 @@ while bool(second_items):
     del original[min_item_index]  # removes item from its original position
     del second_items[min_item_index]  # removes item from second items list after transposition
 
-print('\nPart 1: Sorted list by second element of internal lists:')
+print('\nTask 1: Sorted list by second element of internal lists:')
 print(original)
 
 # Task 2
@@ -20,7 +20,7 @@ for list_ in original:
     dictionary[list_[1]] = [list_[0]] + list_[2:]
     # dictionary[list_[1]] = [list_[i] for i in range(len(list_)) if i != 1]     # alternative version
 
-print('\nPart 2: Created dictionary based on sorted internal lists:')
+print('\nTask 2: Created dictionary based on sorted internal lists:')
 print(dictionary)
 
 # Task 3
@@ -28,7 +28,7 @@ for pair in dictionary.items():  # pass through dictionary pairs
     pair[1].sort(reverse=True)  # sorts lists (dictionary values) in descending order
     dictionary[pair[0]] = pair[1]  # overwrites the value in the dictionary
 
-print('\nPart 3: Sorted dictionary values (lists) in descending order:')
+print('\nTask 3: Sorted dictionary values (lists) in descending order:')
 print(dictionary)
 
 # Task 4
@@ -36,10 +36,10 @@ values_set = set()
 for list_ in dictionary.values():
     values_set = values_set.union(list_)  # unites all dictionary values to set
 
-print('\nPart 4: Created set based on all dictionary values:')
+print('\nTask 4: Created set based on all dictionary values:')
 print(values_set)
 
 # Task 5
 string = str(values_set)
-print('\nPart 5: String based on set:')
+print('\nTask 5: String based on set:')
 print(string)
