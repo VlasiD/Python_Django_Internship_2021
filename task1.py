bce117c6a2855290d44342698177339cf3584f11
Vladimir <vladimirsidenko11@gmail.com>
2021-12-12 17:59:08 +0200

    Initial commit

---
+++ task1.py
@@ -0,0 +1,45 @@
+# Part1
+original = [[1, 3, 3, 4], [2, 1, 3, 5], [4, 0, 1, 7], [5, 2, 1, 0], [0, 4, 8, 3]]
+print('Original list:')
+print(original)
+
+second_items = [list_[1] for list_ in original]  # gets second items of internal lists
+
+while bool(second_items):
+    min_item_index = second_items.index(min(second_items))  # finds index of min element among the second items
+    original.append(original[min_item_index])  # adds item with min 2-nd element to the end of list
+    del original[min_item_index]  # removes item from its original position
+    del second_items[min_item_index]  # removes item from second items list after transposition
+
+print('\nPart 1: Sorted list by second element of internal list:')
+print(original)
+
+# Part2
+dictionary = {}
+for list_ in original:
+    dictionary[list_[1]] = [list_[0]] + list_[2:]
+    # dictionary[list_[1]] = [list_[i] for i in range(len(list_)) if i != 1]     # alternative version
+
+print('\nPart 2: Created dictionary based on sorted internal lists')
+print(dictionary)
+
+# Part3
+for pair in dictionary.items():  # pass through dictionary pairs
+    pair[1].sort(reverse=True)  # sorts lists (dictionary values) in descending order
+    dictionary[pair[0]] = pair[1]  # overwrites the value in the dictionary
+
+print('\nPart 3: Sorted dictionary values (lists) in descending order')
+print(dictionary)
+
+# Part4
+values_set = set()
+for list_ in dictionary.values():
+    values_set = values_set.union(list_)  # unites all dictionary values to set
+
+print('\nPart 4: Created set based on all dictionary values')
+print(values_set)
+
+# Part5
+string = str(values_set)
+print('\nPart 5: String based on set')
+print(string)
